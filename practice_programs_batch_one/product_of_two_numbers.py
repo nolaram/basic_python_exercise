2532279166d345a5a64fabe416fa5aa22d2e9edf
nolaram <marloncopino10@gmail.com>
2025-03-11 23:33:00 +0800

    multiply_the_two_numbers

--- practice_programs_batch_one/product_of_two_numbers.py
+++ practice_programs_batch_one/product_of_two_numbers.py
@@ -3,4 +3,6 @@ number_one = int(input("\nEnter your first number: "))
 number_two = int(input("Enter your second number: "))
 
 # multiply the two numbers
+product = number_one * number_two
+
 # print the product
