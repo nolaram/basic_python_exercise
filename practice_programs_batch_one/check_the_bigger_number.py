cc9a3b30930e184024c401cae8b4ff8eb4605dfe
nolaram <marloncopino10@gmail.com>
2025-03-11 23:02:51 +0800

    check_what_is_the_bigger_number

--- practice_programs_batch_one/check_the_bigger_number.py
+++ practice_programs_batch_one/check_the_bigger_number.py
@@ -4,8 +4,12 @@ number_two = int(input('Enter your second number: '))
 
 # check what is the bigger number
 # if number one is bigger
+if number_one > number_two:
     # print "The bigger number is number one"
+    print(f"\nThe bigger number is: {number_one}")
 # if number two is bigger
+elif number_two > number_one:
     # print "The bigger number is number two"
+    print(f"\nThe bigger number is: {number_two}")
 # if the two numbers are the same
     # print "The two numbers are equal"
