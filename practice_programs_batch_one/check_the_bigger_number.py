367a743ec163f92a2b3540a24ee00f9d65b8afb9
nolaram <marloncopino10@gmail.com>
2025-03-11 22:51:18 +0800

    psuedocode_for_program_one

---
+++ practice_programs_batch_one/check_the_bigger_number.py
@@ -0,0 +1,8 @@
+# input two numbers
+# check what is the bigger number
+# if number one is bigger
+    # print "The bigger number is number one"
+# if number two is bigger
+    # print "The bigger number is number two"
+# if the two numbers are the same
+    # print "The two numbers are equal"
