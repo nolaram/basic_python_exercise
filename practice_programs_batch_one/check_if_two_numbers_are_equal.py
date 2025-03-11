a492bfd5be8d08437b93d51cd03ed6c7ef4389af
nolaram <marloncopino10@gmail.com>
2025-03-11 23:17:31 +0800

    input_two_numbers

--- practice_programs_batch_one/check_if_two_numbers_are_equal.py
+++ practice_programs_batch_one/check_if_two_numbers_are_equal.py
@@ -1,4 +1,7 @@
 # input two numbers
+number_one = int(input("\nEnter your first number: "))
+number_two = int(input("Enter your second number: "))
+
 # check if they are equal
 # if equal
     # print "The two numbers are equal"
