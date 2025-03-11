acbf00eb17f96df2678c03b237365178c10f3634
nolaram <marloncopino10@gmail.com>
2025-03-11 23:48:05 +0800

    first_number_raised_to_the_second_number

--- practice_programs_batch_one/first_number_raised_to_the_second_number.py
+++ practice_programs_batch_one/first_number_raised_to_the_second_number.py
@@ -3,4 +3,6 @@ number_one = int(input("\nEnter your first number: "))
 number_two = int(input("Enter your second number: "))
 
 # the first number raised to the second number
+exponent = number_one ** number_two
+
 # print the result
