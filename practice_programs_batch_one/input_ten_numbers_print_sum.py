a69a7b94ece6b6ef1955a9e4921e14bf298fc059
nolaram <marloncopino10@gmail.com>
2025-03-11 23:55:12 +0800

    add_all_ten_numbers

--- practice_programs_batch_one/input_ten_numbers_print_sum.py
+++ practice_programs_batch_one/input_ten_numbers_print_sum.py
@@ -1,6 +1,10 @@
+total = 0
+
 # input ten numbers
 for number in range(10):
         summation = int(input(f"\nEnter number {number + 1}: "))
 
-# add all ten numbers
+        # add all ten numbers
+        total += summation
+
 # print the sum
