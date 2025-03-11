4b0f2735ea284d85da0eb069dbe7f408c0c01ad8
nolaram <marloncopino10@gmail.com>
2025-03-11 23:47:01 +0800

    input_two_numbers

--- practice_programs_batch_one/first_number_raised_to_the_second_number.py
+++ practice_programs_batch_one/first_number_raised_to_the_second_number.py
@@ -1,3 +1,6 @@
 # input two numbers
+number_one = int(input("\nEnter your first number: "))
+number_two = int(input("Enter your second number: "))
+
 # the first number raised to the second number
 # print the result
