f929e1bdf35675c976a8e5f964573e1e2395b2d2
nolaram <marloncopino10@gmail.com>
2025-03-12 00:03:50 +0800

    check_if_odd

--- practice_programs_batch_one/check_odd_numbers.py
+++ practice_programs_batch_one/check_odd_numbers.py
@@ -1,6 +1,11 @@
+count = 0
+
 # input ten numbers
 for number in range(10):
         odd = int(input(f"\nEnter number {number + 1}: "))
-        
-# check if odd
+
+        # check if odd
+        if odd % 2 != 0:
+                    count += 1
+
 # print how many are the odd numbers
