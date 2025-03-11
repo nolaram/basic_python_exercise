6580b23b3e545a34dfba9edd2e92105576a2e80a
nolaram <marloncopino10@gmail.com>
2025-03-11 23:42:46 +0800

    added_a_program_so_the_user_cannot_devide_by_zero

--- practice_programs_batch_one/quotient_of_two_numbers_with_decimal_point.py
+++ practice_programs_batch_one/quotient_of_two_numbers_with_decimal_point.py
@@ -1,9 +1,14 @@
-# input two numbers
-number_one = int(input("\nEnter your first number: "))
-number_two = int(input("Enter your second number: "))
+try:
+    # input two numbers
+    number_one = int(input("\nEnter your first number: "))
+    number_two = int(input("Enter your second number: "))
 
-# divide the two numbers with the decimal point on
-quotient = number_one / number_two
+    # divide the two numbers with the decimal point on
+    quotient = number_one / number_two
 
-# print the quotient
-print(f"\nThe quotient of the two numbers is: {quotient}")
+    # print the quotient
+    print(f"\nThe quotient of the two numbers is: {quotient}")
+
+# added so that the user cannot devide by zero
+except ZeroDivisionError:
+    print("\nYou cannot devide by zero!")
