6fd6c0b31dabb5d4bec910bb70df765a24b5bb43
nolaram <marloncopino10@gmail.com>
2025-03-12 19:51:40 +0800

    stop_program_if_not_numerical

--- practice_programs_batch_four/sort_highest_to_lowest.py
+++ practice_programs_batch_four/sort_highest_to_lowest.py
@@ -9,8 +9,12 @@ while True:
 
         # store input numbers
         store_input_number.append(input_number)
-        
-# stop the program if not numerical
+
+    # stop the program if not numerical
+    except ValueError:
+        print("The input must be numerical")
+        break
+
 # exit loop and sort the list to highest to lowest
     # print the result
 # if no numbers were entered
