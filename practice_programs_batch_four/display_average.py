e87cecd42d8637270d0165494165867c5ad83b5b
nolaram <marloncopino10@gmail.com>
2025-03-12 20:03:27 +0800

    store_input_to_list

--- practice_programs_batch_four/display_average.py
+++ practice_programs_batch_four/display_average.py
@@ -6,6 +6,7 @@ while True:
     try:
         input_number = int(input("Enter a number: "))
     # store input number to list
+    store_input_number.append(input_number)
 # stop program if not numerical
 # exit loop and check the average of the numbers in the list
     # print the result
