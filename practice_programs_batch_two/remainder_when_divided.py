64d54d380791848c6f9f7e4a7162de6f8e5d3ee1
nolaram <marloncopino10@gmail.com>
2025-03-12 09:56:53 +0800

    divide_two_numbers

--- practice_programs_batch_two/remainder_when_divided.py
+++ practice_programs_batch_two/remainder_when_divided.py
@@ -3,4 +3,6 @@ number_one = int(input("\nEnter your first number: "))
 number_two = int(input("Enter your second number: "))
 
 # divide the two numbers
+remainder = number_one % number_two
+
 # print the remainder
