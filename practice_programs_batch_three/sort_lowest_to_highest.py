fbdc439bfefa6fa147f04ac7558cad1429c25af1
nolaram <marloncopino10@gmail.com>
2025-03-12 16:26:16 +0800

    if_no_numbers_entered_display_none

--- practice_programs_batch_three/sort_lowest_to_highest.py
+++ practice_programs_batch_three/sort_lowest_to_highest.py
@@ -5,22 +5,24 @@ stored_input_number = []
 while True:
     try:
 
-        input_number = int(input("Enter a number: "))
+        input_number = int(input("\nEnter a number: "))
 
         # store input number to list
         stored_input_number.append(input_number)
 
     # stops program if not numerical value
     except ValueError:
-        print("The input must be numerical")
+        print("\nThe input must be numerical")
         break
 
 # exits loop and sorts the input numbers stored in list
 if stored_input_number:
     stored_input_number.sort()
     # print the result
-    print("The entered numbers from lowest to highest is:")
+    print("\nThe entered numbers from lowest to highest is:")
     for input_number in stored_input_number:
         print(input_number)
 # if no numbers were entered
+else:
     # print none
+    print("\nThere were no numbers entered")
