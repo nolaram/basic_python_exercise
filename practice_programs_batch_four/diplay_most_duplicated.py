12df17704f16f3f59f5a345e8ccae538d09888f1
nolaram <marloncopino10@gmail.com>
2025-03-12 19:06:36 +0800

    loop_to_ask_input

--- practice_programs_batch_four/diplay_most_duplicated.py
+++ practice_programs_batch_four/diplay_most_duplicated.py
@@ -1,6 +1,10 @@
 # list of input numbers
 store_input_number = []
+
 # loop to ask input
+while True:
+    try:
+        input_number = int(input("Enter a number: "))
     # store input numbers to list
 # stop program if input is not numerical
 # dictionary of every number input
