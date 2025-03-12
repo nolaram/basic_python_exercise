64fb284d5f6b01df09a23b6f01b0594bbc2a0955
nolaram <marloncopino10@gmail.com>
2025-03-12 15:18:18 +0800

    check_if_duplicated_and_print_result

--- practice_programs_batch_three/display_only_first_duplicate.py
+++ practice_programs_batch_three/display_only_first_duplicate.py
@@ -3,10 +3,15 @@ check_seen_numbers = set() # set to track numbers
 
 # input ten numbers
 for number in range(10):
-    input_number = int(input(f"Enter a number {number + 1}: "))
+    input_number = int(input(f"\nEnter a number {number + 1}: "))
     # store the input number to the list
     store_input_numbers.append(input_number)
 
 # check if the number is duplicated for the first occurance
+print("\nThe number results are:")
+for input_number in store_input_numbers:
+    if input_number not in check_seen_numbers:
+        # print the result
+        print(input_number)
 # ignore all numbers that are all ready seen by the set
-# print the result
+
