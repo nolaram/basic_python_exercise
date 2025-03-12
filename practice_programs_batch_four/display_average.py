1e30efa21b9b39006eff49fc5208b20203be8e9c
nolaram <marloncopino10@gmail.com>
2025-03-12 20:09:27 +0800

    no_numbers_were_entered

--- practice_programs_batch_four/display_average.py
+++ practice_programs_batch_four/display_average.py
@@ -4,18 +4,20 @@ store_input_number = []
 # loop to ask for input numbers
 while True:
     try:
-        input_number = int(input("Enter a number: "))
+        input_number = int(input("\nEnter a number: "))
         # store input number to list
         store_input_number.append(input_number)
     # stop program if not numerical
     except ValueError:
-        print("The input must be numerical")
+        print("\nThe input must be numerical")
         break
 
 # exit loop and check the average of the numbers in the list
 if store_input_number:
     average = sum(store_input_number) / len(store_input_number)
     # print the result
-    print(f"The average of all numbers is: {average}")
+    print(f"\nThe average of all numbers is: {average}")
 # if no numbers were entered
+else:
     # print none
+    print("\nThere were no numbers entered")
