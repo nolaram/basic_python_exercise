1ee70e23463ee330cb20f8270c40126fc03b8352
nolaram <marloncopino10@gmail.com>
2025-03-12 09:36:22 +0800

    input_two_numbers

--- practice_programs_batch_two/not_equal_when_two_numbers_are_not_same.py
+++ practice_programs_batch_two/not_equal_when_two_numbers_are_not_same.py
@@ -1,4 +1,7 @@
 # input two numbers
+number_one = int(input("\nEnter your first number: "))
+number_two = int(input("Enter your second number: "))
+
 # check if not equal
 # if not equal
     # print "The two numbers are not equal"
