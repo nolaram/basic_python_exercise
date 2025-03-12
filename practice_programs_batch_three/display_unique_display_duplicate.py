fd659d240c0d28ac7ba54fd7e9871612c75575fb
nolaram <marloncopino10@gmail.com>
2025-03-12 15:29:05 +0800

    pseudocode_for_program_three

---
+++ practice_programs_batch_three/display_unique_display_duplicate.py
@@ -0,0 +1,6 @@
+# list to store duplicate numbers
+# a loop that asks to input a number
+    # store the input number to the list 
+    # print duplicate if the number has a same number in the list
+    # print unique if the number has no same number in the list
+# stops the program if not numerical value
