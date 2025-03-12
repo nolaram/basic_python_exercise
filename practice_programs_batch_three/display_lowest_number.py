c3888f42c4743488013ad12d6df702afbdc63a2e
nolaram <marloncopino10@gmail.com>
2025-03-12 16:04:02 +0800

    if_no_number_entered

--- practice_programs_batch_three/display_lowest_number.py
+++ practice_programs_batch_three/display_lowest_number.py
@@ -5,17 +5,19 @@ stored_input_number = []
 while True:
     try:
         
-        input_number = int(input("Enter a number: "))
+        input_number = int(input("\nEnter a number: "))
 
         # store input number
         stored_input_number.append(input_number)
 
     # stop program if the input is not numerical
     except:
-        print("The input must be numerical")
+        print("\nThe input must be numerical")
         break
 
 # exit the loop and print the lowest number stored in the list
 if stored_input_number:
-    print("The lowest number is:", min(stored_input_number))
+    print("\nThe lowest number is:", min(stored_input_number))
 # if no numbers were entered, print none 
+else:
+    print("\nNo numbers were entered")
