c6288881500669305377e2f5ee786fe4b633b15f
nolaram <marloncopino10@gmail.com>
2025-03-12 16:16:39 +0800

    pseudocode_for_program_five

---
+++ practice_programs_batch_three/sort_lowest_to_highest.py
@@ -0,0 +1,8 @@
+# list of stored input numbers
+# loop to ask for input number
+    # store input number to lost
+# stops program if not numerical value
+# exits loop and sorts the input numbers stored in list
+    # print the result
+# if no numbers were entered
+    # print none
