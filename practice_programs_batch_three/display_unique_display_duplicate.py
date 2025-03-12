202649893938a2234caeeb1f2377529944d739d5
nolaram <marloncopino10@gmail.com>
2025-03-12 15:35:53 +0800

    check_if_unique_and_print_if_true

--- practice_programs_batch_three/display_unique_display_duplicate.py
+++ practice_programs_batch_three/display_unique_display_duplicate.py
@@ -6,9 +6,11 @@ while True:
     try:
         input_number = int(input("Enter a number: ")) 
 
-    if input_number in store_input_number: 
-        # print duplicate if the number has a same number in the list
-        print("Duplicate")
-    # print unique if the number has no same number in the list
+        if input_number in store_input_number: 
+            # print duplicate if the number has a same number in the list
+            print("Duplicate")
+        else:
+            # print unique if the number has no same number in the list
+            print("Unique")
     # store the input number to the list
 # stops the program if not numerical value
