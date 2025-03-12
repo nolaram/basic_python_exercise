efe2473d822d17fdef4284f307b98e8c5ee5007a
nolaram <marloncopino10@gmail.com>
2025-03-12 20:01:05 +0800

    pseudocode_for_program_five

---
+++ practice_programs_batch_four/display_average.py
@@ -0,0 +1,8 @@
+# list stored input numbers
+# loop to ask for input numbers
+    # store input number to list
+# stop program if not numerical
+# exit loop and check the average of the numbers in the list
+    # print the result
+# if no numbers were entered
+    # print none
