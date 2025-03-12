877c7a53d1be0763ca24c3a5709e85fdc22fe2ee
nolaram <marloncopino10@gmail.com>
2025-03-12 15:06:40 +0800

    pseudocode_for_program_two

---
+++ practice_programs_batch_three/display_only_first_duplicate.py
@@ -0,0 +1,7 @@
+# list for input numbers
+# set to track numbers
+# input ten numbers
+# store the input number to the list
+# check if the number is duplicated for the first occurance
+# ignore all numbers that are all ready seen by the set
+# print the result
