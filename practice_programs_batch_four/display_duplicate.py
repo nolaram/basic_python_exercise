02e7039e7085886831998906b23e0fa92efaac3a
nolaram <marloncopino10@gmail.com>
2025-03-12 18:40:14 +0800

    pseudocode_for_program_one

---
+++ practice_programs_batch_four/display_duplicate.py
@@ -0,0 +1,8 @@
+# list of stored input numbers
+# input ten numbers
+    # store input numbers to list
+# list of duplicate numbers 
+# check if duplicate and put to list
+# print the result
+# if no numbers entered
+    # print none
