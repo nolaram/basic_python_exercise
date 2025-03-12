263ac109b56cdc701c99d199f86c683e90fab357
nolaram <marloncopino10@gmail.com>
2025-03-12 20:05:04 +0800

    stop_program_if_not_numerical

--- practice_programs_batch_four/display_average.py
+++ practice_programs_batch_four/display_average.py
@@ -5,9 +5,13 @@ store_input_number = []
 while True:
     try:
         input_number = int(input("Enter a number: "))
-    # store input number to list
-    store_input_number.append(input_number)
-# stop program if not numerical
+        # store input number to list
+        store_input_number.append(input_number)
+    # stop program if not numerical
+    except ValueError:
+        print("The input must be numerical")
+        break
+
 # exit loop and check the average of the numbers in the list
     # print the result
 # if no numbers were entered
