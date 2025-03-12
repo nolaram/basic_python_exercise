180dd211fe02eaf3933f4a090470f678bf91a668
nolaram <marloncopino10@gmail.com>
2025-03-12 20:02:37 +0800

    loop_to_ask_input

--- practice_programs_batch_four/display_average.py
+++ practice_programs_batch_four/display_average.py
@@ -2,6 +2,9 @@
 store_input_number = []
 
 # loop to ask for input numbers
+while True:
+    try:
+        input_number = int(input("Enter a number: "))
     # store input number to list
 # stop program if not numerical
 # exit loop and check the average of the numbers in the list
