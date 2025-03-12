cb6fd2b6a37f9b1e78ea61b43d9977c922897d50
nolaram <marloncopino10@gmail.com>
2025-03-12 09:24:07 +0800

    pseudocode_for_program_one

---
+++ practice_programs_batch_two/check_the_smaller_number.py
@@ -0,0 +1,8 @@
+# input two numbers
+# check what is the smaller number
+# if number one is the smaller number
+    # print "The smaller number is number one"
+# if number two is the smaller number 
+    # print "The smaller number is number two"
+# if the numbers are the same
+    # print "The numbers are equal"
