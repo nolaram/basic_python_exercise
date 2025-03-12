248ce00984c586f58e973e0618e289858ff6222e
nolaram <marloncopino10@gmail.com>
2025-03-12 09:42:28 +0800

    pseudocode_for_program_three_and_input_two_numbers

---
+++ practice_programs_batch_two/difference_of_two_numbers.py
@@ -0,0 +1,6 @@
+# input two numbers
+number_one = int(input("\nEnter your first number: "))
+number_two = int(input("Enter your second number: "))
+
+# subtract the two numbers
+# print the difference
