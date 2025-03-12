58a9d26c092bb314bafc7facc1ce7959e486f615
nolaram <marloncopino10@gmail.com>
2025-03-12 10:08:18 +0800

    subtract_to_the_first_number

--- practice_programs_batch_two/first_number_minus_all.py
+++ practice_programs_batch_two/first_number_minus_all.py
@@ -1,6 +1,10 @@
+total = 0
+
 # input ten numbers
 for number in range(10):
     difference = int(input(f"\nEnter number {number + 1}: "))
-    
+
     # subtract all to the first number
+    total -= difference
+    
 # print the difference
