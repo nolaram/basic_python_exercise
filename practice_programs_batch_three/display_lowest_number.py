158d20bb560b154b2638948c73a2de917e03c1f7
nolaram <marloncopino10@gmail.com>
2025-03-12 15:58:01 +0800

    loop_to_ask_input

--- practice_programs_batch_three/display_lowest_number.py
+++ practice_programs_batch_three/display_lowest_number.py
@@ -2,7 +2,12 @@
 stored_input_number = []
 
 # loop to ask for input number
-    # store input number
+while True:
+    try:
+        
+        input_number = int(input("Enter a number: "))
+
+        # store input number
 # stop program if the input is not numerical
 # exit the loop and print the lowest number stored in the list
 # if no numbers were entered, print none 
