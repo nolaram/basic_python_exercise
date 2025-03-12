3d3dbeaaacbb017ff8bc5a31f7110b5238db2ce5
nolaram <marloncopino10@gmail.com>
2025-03-12 18:47:50 +0800

    input_ten_numbers_store_to_list

--- practice_programs_batch_four/display_duplicate.py
+++ practice_programs_batch_four/display_duplicate.py
@@ -2,7 +2,11 @@
 store_input_number = []
 
 # input ten numbers
+for number in range(10):
+    input_number = int(input(f"Enter number {number + 1}: "))
     # store input numbers to list
+    store_input_number.append(input_number)
+    
 # list of duplicate numbers 
 # check if duplicate and put to list
 # print the result
