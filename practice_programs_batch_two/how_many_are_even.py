230f2db8f43ee6c974929e9422ca917b9386723a
nolaram <marloncopino10@gmail.com>
2025-03-12 10:19:53 +0800

    check_if_even

--- practice_programs_batch_two/how_many_are_even.py
+++ practice_programs_batch_two/how_many_are_even.py
@@ -1,6 +1,11 @@
+count = 0
+
 # input ten numbers
 for number in range(10):
-    even = int(input(f"Enter number {number + 1}: "))
-    
-# check if even number
+    even = int(input(f"\nEnter number {number + 1}: "))
+
+    # check if even number
+    if even % 2 == 0:
+        count += 1
+
 # print how many is an even number
