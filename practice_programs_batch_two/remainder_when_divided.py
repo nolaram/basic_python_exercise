476f9995dc21ea821a02cb1913cc7c31933e1cbb
nolaram <marloncopino10@gmail.com>
2025-03-12 09:59:59 +0800

    print_result_and_added_zero_division_error

--- practice_programs_batch_two/remainder_when_divided.py
+++ practice_programs_batch_two/remainder_when_divided.py
@@ -1,8 +1,14 @@
-# input two numbers
-number_one = int(input("\nEnter your first number: "))
-number_two = int(input("Enter your second number: "))
+try:
+    # input two numbers
+    number_one = int(input("\nEnter your first number: "))
+    number_two = int(input("Enter your second number: "))
 
-# divide the two numbers
-remainder = number_one % number_two
+    # divide the two numbers
+    remainder = number_one % number_two
 
-# print the remainder
+    # print the remainder
+    print(f"\nThe quotient of the two numbers is: {remainder}")
+
+# added so that the user cannot devide by zero
+except ZeroDivisionError:
+     print("\nYou cannot devide by zero!")
