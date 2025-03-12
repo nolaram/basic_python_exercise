950471f81c4f2c2b93fbdc7fad541fdd63098135
nolaram <marloncopino10@gmail.com>
2025-03-12 15:12:11 +0800

    input_ten_numbers_and_store_to_list

--- practice_programs_batch_three/display_only_first_duplicate.py
+++ practice_programs_batch_three/display_only_first_duplicate.py
@@ -1,7 +1,12 @@
-# list for input numbers
-# set to track numbers
+store_input_numbers = [] # list for input numbers
+check_seen_numbers = set() # set to track numbers
+
 # input ten numbers
-# store the input number to the list
+for number in range(10):
+    input_number = int(input(f"Enter a number {number + 1}: "))
+    # store the input number to the list
+    store_input_numbers.append(input_number)
+
 # check if the number is duplicated for the first occurance
 # ignore all numbers that are all ready seen by the set
 # print the result
