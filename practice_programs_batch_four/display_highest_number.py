1695fbd81d813b4c9cf490a829bfbc6accb1f30b
nolaram <marloncopino10@gmail.com>
2025-03-12 19:34:11 +0800

    pseudocode_for_program_three

---
+++ practice_programs_batch_four/display_highest_number.py
@@ -0,0 +1,8 @@
+# list of input numbers
+# loop to ask input
+    # store input number
+# stop program if input is not numerical
+# exit loop and find the highest number in list
+    # print the result
+# if no numbers were entered
+    # print none
