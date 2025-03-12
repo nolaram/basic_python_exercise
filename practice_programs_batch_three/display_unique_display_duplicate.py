cf71951f9c2e506d27102f8161ab2ce4ad881394
nolaram <marloncopino10@gmail.com>
2025-03-12 15:32:37 +0800

    loop_to_input_number

--- practice_programs_batch_three/display_unique_display_duplicate.py
+++ practice_programs_batch_three/display_unique_display_duplicate.py
@@ -2,7 +2,10 @@
 store_input_number = []
 
 # a loop that asks to input a number
-    # store the input number to the list 
+while True:
+    try:
+        input_number = int(input("Enter a number: "))  
     # print duplicate if the number has a same number in the list
     # print unique if the number has no same number in the list
+    # store the input number to the list
 # stops the program if not numerical value
