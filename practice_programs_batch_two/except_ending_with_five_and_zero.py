ede5cee699cf5bf6a1b8998c9bb588716d9e8132
nolaram <marloncopino10@gmail.com>
2025-03-12 10:39:00 +0800

    check_if_ending_with_five

--- practice_programs_batch_two/except_ending_with_five_and_zero.py
+++ practice_programs_batch_two/except_ending_with_five_and_zero.py
@@ -1,7 +1,7 @@
 # print zero to one hundred
 for number in range(101):
-
-# check if ending with five
-# check if ending with zero
-    # print the number
-    print(number)
+    # check if ending with five
+    if number % 10 != 5:
+    # check if ending with zero
+        # print the number
+        print(number)
