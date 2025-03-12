4a6293f30e3772ceb33b5e8e5af94339e8325489
nolaram <marloncopino10@gmail.com>
2025-03-12 09:26:14 +0800

    input_two_numbers

--- practice_programs_batch_two/check_the_smaller_number.py
+++ practice_programs_batch_two/check_the_smaller_number.py
@@ -1,4 +1,7 @@
 # input two numbers
+num1 = int(input('Enter your first number: '))
+num2 = int(input('Enter your second number: '))
+
 # check what is the smaller number
 # if number one is the smaller number
     # print "The smaller number is number one"
