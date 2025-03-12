0c19cffff70d484ff3e454c48a4d585298eeb247
nolaram <marloncopino10@gmail.com>
2025-03-12 13:54:07 +0800

    store_input_number

--- practice_programs_batch_three/display_no_duplicate.py
+++ practice_programs_batch_three/display_no_duplicate.py
@@ -1,7 +1,11 @@
 store_input_number = []
+
 # input ten numbers 
 for number in range(10):
     input_number = int(input(f"\nEnter a number {number + 1}: "))
+    # store the input number to the variable
+    store_input_number.append(number)
 
 # check if input number is unique
+
 # print the result
