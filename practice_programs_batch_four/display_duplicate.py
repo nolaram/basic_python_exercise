7ecc5aa2da438519b6208b7a79e151e13d5944bf
nolaram <marloncopino10@gmail.com>
2025-03-12 18:52:04 +0800

    print_the_result

--- practice_programs_batch_four/display_duplicate.py
+++ practice_programs_batch_four/display_duplicate.py
@@ -14,7 +14,12 @@ duplicate_number = []
 for input_number in store_input_number:
     if store_input_number.count(input_number) > 1 and input_number not in duplicate_number:
         duplicate_number.append(input_number)
-        
+
 # print the result
+print("The numbers that are duplicated are:")
+if duplicate_number:
+    for input_number in duplicate_number:
+        print(input_number)
 # if no numbers entered
+
     # print none
