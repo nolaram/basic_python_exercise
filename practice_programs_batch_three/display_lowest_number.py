4b7ed77c5c31d5b5b10350184c8afc6f8acbdb44
nolaram <marloncopino10@gmail.com>
2025-03-12 15:55:15 +0800

    pseudocode_for_program_four

---
+++ practice_programs_batch_three/display_lowest_number.py
@@ -0,0 +1,6 @@
+# list of stored input numbers
+# loop to ask for input number
+    # store input number
+# stop program if the input is not numerical
+# exit the loop and print the lowest number stored in the list
+# if no numbers were entered, print none 
