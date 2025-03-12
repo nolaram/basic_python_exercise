5a2e041103adc8fe004e2a14053ec605ce2a86d6
nolaram <marloncopino10@gmail.com>
2025-03-12 19:04:27 +0800

    pseudocode_for_program_two

---
+++ practice_programs_batch_four/diplay_most_duplicated.py
@@ -0,0 +1,8 @@
+# list of input numbers
+# loop to ask input
+    # store input numbers to list
+# stop program if input is not numerical
+# dictionary of every number input
+# count the frequency of number input
+# find the most frequent number input
+# print the result
