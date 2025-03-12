7db1f7b600a805bc72b57dbd2e3b65fcada83823
nolaram <marloncopino10@gmail.com>
2025-03-12 15:40:39 +0800

    stops_program_if_input_not_numerical

--- practice_programs_batch_three/display_unique_display_duplicate.py
+++ practice_programs_batch_three/display_unique_display_duplicate.py
@@ -4,14 +4,20 @@ store_input_number = []
 # a loop that asks to input a number
 while True:
     try:
-        input_number = int(input("Enter a number: ")) 
+
+        input_number = int(input("\nEnter a number: ")) 
 
         if input_number in store_input_number: 
             # print duplicate if the number has a same number in the list
-            print("Duplicate")
+            print("\nDuplicate")
         else:
             # print unique if the number has no same number in the list
-            print("Unique")
+            print("\nUnique")
+            
         # store the input number to the list
         store_input_number.append(input_number)
-# stops the program if not numerical value
+
+    # stops the program if not numerical value
+    except ValueError:
+        print("\nThe input must be numerical!")
+        break
