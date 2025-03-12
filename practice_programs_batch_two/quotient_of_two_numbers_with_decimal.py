53fa91de6999f18b011559d04d298ced47f82f00
nolaram <marloncopino10@gmail.com>
2025-03-12 09:50:08 +0800

    divide_with_decimal

--- practice_programs_batch_two/quotient_of_two_numbers_with_decimal.py
+++ practice_programs_batch_two/quotient_of_two_numbers_with_decimal.py
@@ -3,4 +3,6 @@ number_one = int(input("\nEnter your first number: "))
 number_two = int(input("Enter your second number: "))
 
 # divide the two numbers with the decimal point
+quotient = number_one // number_two
+
 # print the result
