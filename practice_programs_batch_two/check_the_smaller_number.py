868fca7c412f0e1e4536395caa0d4437df3c27eb
nolaram <marloncopino10@gmail.com>
2025-03-12 09:30:36 +0800

    check_the_smaller_number_and_fixed_variable_names

--- practice_programs_batch_two/check_the_smaller_number.py
+++ practice_programs_batch_two/check_the_smaller_number.py
@@ -1,11 +1,15 @@
 # input two numbers
-num1 = int(input('Enter your first number: '))
-num2 = int(input('Enter your second number: '))
+number_one = int(input('\nEnter your first number: '))
+number_two = int(input('Enter your second number: '))
 
 # check what is the smaller number
 # if number one is the smaller number
+if number_one < number_two:
     # print "The smaller number is number one"
+    print(f"\nThe smaller number is: {number_one}")
 # if number two is the smaller number 
+elif number_two < number_one:
     # print "The smaller number is number two"
+    print(f"\nThe smaller number is: {number_two}")
 # if the numbers are the same
     # print "The numbers are equal"
