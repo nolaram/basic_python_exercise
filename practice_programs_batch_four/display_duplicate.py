840d6b339c890266debbe50778748e68ab97e427
nolaram <marloncopino10@gmail.com>
2025-03-12 18:50:22 +0800

    check_if_duplicate

--- practice_programs_batch_four/display_duplicate.py
+++ practice_programs_batch_four/display_duplicate.py
@@ -6,9 +6,15 @@ for number in range(10):
     input_number = int(input(f"Enter number {number + 1}: "))
     # store input numbers to list
     store_input_number.append(input_number)
-    
+
 # list of duplicate numbers 
+duplicate_number = []
+
 # check if duplicate and put to list
+for input_number in store_input_number:
+    if store_input_number.count(input_number) > 1 and input_number not in duplicate_number:
+        duplicate_number.append(input_number)
+        
 # print the result
 # if no numbers entered
     # print none
