9a80bf8434eeb412387a1861d30dd8230e9b0847
nolaram <marloncopino10@gmail.com>
2025-03-12 19:41:44 +0800

    if_no_numbers_added_print_none

--- practice_programs_batch_four/display_highest_number.py
+++ practice_programs_batch_four/display_highest_number.py
@@ -4,18 +4,20 @@ store_input_numbers = []
 # loop to ask input
 while True:
     try:
-        input_number = int(input("Enter a number: "))
+        input_number = int(input("\nEnter a number: "))
         # store input number
         store_input_numbers.append(input_number)
 
     # stop program if input is not numerical
     except ValueError:
-        print("The input must be numerical")
+        print("\nThe input must be numerical")
         break
 
 # exit loop and find the highest number in list
 if store_input_numbers:
     # print the result
-    print("The highest number recorded was:", max(store_input_numbers))   
+    print("\nThe highest number recorded was:", max(store_input_numbers))   
 # if no numbers were entered
+else:
     # print none
+    print("\nThere were no numbers added")
